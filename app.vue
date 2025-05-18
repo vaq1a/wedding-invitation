8f38e95010be907068f1b2755010d5782db5738f
vaq1a <vadimis26@gmail.com>
2025-05-18 22:49:36 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header>
+      Header content
+    </header>
+    <main>
+      Main content
+    </main>
+    <footer>
+      Footer content
+    </footer>
+  </div>
+</template>
